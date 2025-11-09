15826d462e9137747c0bb6a8a95b1cf52e143caf
Mr Pineapple <pineapple123132@gmail.com>
2025-11-09 16:35:23 +0500

    RAYLIB WORKS!

---
+++ run.bat
@@ -0,0 +1,20 @@
+@echo off
+REM Compile main.cpp using g++
+g++ src\main.cpp -o src\main -I"C:\msys64\ucrt64\include" -L"C:\msys64\ucrt64\lib" -lraylib -lopengl32 -lgdi32 -lwinmm
+
+REM Check if compilation succeeded
+if %errorlevel% neq 0 (
+    echo.
+    echo Compilation failed!
+    pause
+    exit /b %errorlevel%
+)
+
+echo.
+echo Compilation successful. Running program...
+echo.
+
+REM Run the compiled program
+.\src\main.exe
+
+echo.
